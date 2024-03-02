7265fcfba1aa04424b00c544bbe7ec5c2bb3e005
heppaat <pali.herrer@gmail.com>
2024-03-02 15:33:38 +0100

    buttons and divs have variables

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,1 +1,17 @@
 import "./style.css";
+import { z } from "zod";
+
+const registrationForm = document.getElementById(
+  "registrationForm"
+) as HTMLDivElement;
+const emailField = document.getElementById("email") as HTMLInputElement;
+const passwordField = document.getElementById("password") as HTMLInputElement;
+const passwordConfirmationField = document.getElementById(
+  "passwordConfirmation"
+) as HTMLInputElement;
+const registerButton = document.getElementById("register") as HTMLButtonElement;
+const successDiv = document.getElementById("successDiv") as HTMLDivElement;
+const backToHomePageButton = document.getElementById(
+  "backToHomepage"
+) as HTMLButtonElement;
+const errorDiv = document.getElementById("errorDiv") as HTMLDivElement;
